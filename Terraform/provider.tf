3086212c571e14a2570cc0f8abfb090e554cb538
Ryan Leary <ryanleary@mac.mynetworksettings.com>
2025-08-09 08:50:28 -0400

    remote backend & ansible steps

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -1,3 +1,21 @@
 provider "aws" {
     region = "us-east-1"
 }
+
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.23.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "level_one_bucket"
+    key            = "tf-state-setup"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "level-one-table"
+  }
+}
--- Terraform/variable.tf
+++ Terraform/variable.tf
@@ -2,3 +2,23 @@ variable "instance_type" {
     default = "t2.micro"
     description = "type of ec2 instance"
 }
+
+variable "tf_state_bucket" {
+  description = "Name of the S3 bucket in AWS for storing TF state"
+  default     = "level-one-bucket"
+}
+
+variable "tf_state_lock_table" {
+  description = "Name of the DynamoDB table for TF state locking"
+  default     = "level-one-lock-table"
+}
+
+variable "project" {
+  description = "Project name of tagging resources"
+  default     = "recipe-app-api"
+}
+
+variable "contact" {
+  description = "contact name for tagging resources"
+  default     = "73.leary@gmail.com"
+}
