65f430692b3df05c7f7ef60d50fa6d43955c1358
RyanLeary2764 <learcapital24@gmail.com>
2025-09-02 21:12:53 -0400

    Update variables.tf

---
+++ Terraform/modules/vpc-snet/variables.tf
@@ -0,0 +1,9 @@
+variable "project" {
+  description = "Project name for tagging"
+  type        = string
+}
+
+variable "contact" {
+  description = "Contact name/email for tagging"
+  type        = string
+}
